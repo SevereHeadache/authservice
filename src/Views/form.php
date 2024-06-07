11edad2011fcffe6bf521bdaaeb6021631664474
SevereHeadache <oysterpost@proton.me>
2024-06-07 11:19:12 +0300

    feat: added form submition

--- src/Views/form.php
+++ src/Views/form.php
@@ -48,12 +48,37 @@
 <body>
     <div class="container">
         <div class="form">
-            <form method="post">
+            <form id="auth" method="post">
                 <input name="name" required type="text" placeholder="name"/>
                 <input name="password" required type="password" placeholder="password"/>
                 <button type="submit">sign in</button>
             </form>
         </div>
     </div>
+
+    <script>
+        document.getElementById('auth').addEventListener('submit', function(e){
+            e.preventDefault();
+            const formData = new FormData(this);
+            const request = new XMLHttpRequest();
+            request.onreadystatechange = function() {
+                if (request.readyState == XMLHttpRequest.DONE) {
+                    if (request.status === 200) {
+                        const jwt = JSON.parse(request.responseText).access_token;
+                        if (jwt) {
+                            document.cookie = 'Authorization=' + jwt + '; path=/';
+                            location.reload();
+                        }
+                    } else {
+                        alert(request.status);
+                    }
+                }
+            }
+            request.open('POST', '');
+            request.setRequestHeader('Auth-Service-Form', '1');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(Object.fromEntries(formData)));
+        });
+    </script>
 </body>
 </html>
